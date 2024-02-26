792ef3a952d9ac72f1a38bc17baa5bb45ec0cfef
Dev EhChad <devehchad@gmail.com>
2024-02-26 00:18:49 -0500

    Initial Commit
    Basic structure to test bot. First actually commit
    
    first attempt on slash commands
    
    Built my first slash command
    
    Command handler/builder
    
    This update brought a massive update to the bot. I've added a command handler and builder.
    
    Command handler/builder
    
    This update brought a massive update to the bot. I've added a command handler and builder.
    
    Minor Improvements
    
    Changed a few file things
    
    Updated Readme
    
    Update the contents in the Readme file
    
    updated Readme Again
    
    Updated small tweaks
    
    Fixed Command Handler
    
    Minor Updates
    
    Added some files
    
    Update .env-Example
    
    Added Timeout.js
    
    Added a new command and also fixed a few errors
    
    Update config.json
    
    Update config.json
    
    mongodb
    
    Delete config.json

---
+++ src/events/ready/consoleLog.js
@@ -0,0 +1,29 @@
+const { Activity, ActivityType } = require("discord.js");
+
+let status = [
+    {
+        name: 'EhChadServices',
+        type: ActivityType.Streaming,
+        url: 'https://twitch.tv/ehchad'
+    },
+    {
+        name: 'Watch EhChad Live!',
+        type: ActivityType.Streaming,
+        url: 'https://twitch.tv/ehchad'
+    },
+    {
+        name: 'Follow EhChad on Twitch!',
+        type: ActivityType.Streaming,
+        url: 'https://twitch.tv/ehchad'
+    },
+]
+
+
+module.exports = (client) => {
+    console.log(`${client.user.username} is finally now online!`);
+
+    setInterval(() => {
+        let random = Math.floor(Math.random() * status.length);
+        client.user.setActivity(status[random]);
+    }, 5000);
+};
