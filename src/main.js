950b14df893684bab6a811a0852376de6ee7d245
Dev EhChad <devehchad@gmail.com>
2024-02-27 17:27:57 -0500

    Minor Updates
    Added some files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
@@ -10,36 +10,6 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent,
     ],
 });
-
-// Old Interaction Message commands
-/*client.on('interactionCreate', async (interaction) => {
-    try {
-        if (interaction.isButton()) return;
-    await interaction.deferReply({ ephemeral: true });
-
-    const role = interaction.guild.roles.cache.get(interaction.customid);
-    if (!role) {
-        interaction.editReply({
-            content: "I couldn't find that role.",
-        })
-        return;
-    }
-
-    const hasRole = interaction.member.roles.cache.has(role.id);
-    
-    if (hasRole) {
-        await interaction.member.roles.remove(role);
-        await interaction.editReply(`The role ${role} has been removed.`);
-        return;
-    }
-    
-    await interaction.member.roles.add(role);
-    await interaction.editReply(`The role ${role} has been added.`);
-
-    } catch (error) {
-        console.log(error);
-    }
-});*/
 
 eventHandler(client);
 
