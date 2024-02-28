97dbd6f3e72908ec7e52945dfa4c82f225a93326
Dev EhChad <devehchad@gmail.com>
2024-02-27 19:18:34 -0500

    mongodb

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
@@ -11,6 +12,16 @@ const client = new Client({
     ],
 });
 
-eventHandler(client);
+(async () => {
+    try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Conneted to EhChadServices DB.');
 
-client.login(process.env.TOKEN);
+    eventHandler(client);
+
+    client.login(process.env.TOKEN);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})();
